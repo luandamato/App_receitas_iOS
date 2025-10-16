e483ca12fcfef971489db3be715e7767e3fc1fbc
Luan Damato <luandamato@gmail.com>
2025-10-16 10:50:38 -0400

    first commit

---
+++ ReceitasApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReceitasApp
+//
+//  Created by Luan Damato on 16/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
